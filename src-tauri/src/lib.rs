2942b5461f9f45614872c10ca9df734b4d37b9b5
so1ve <i@mk1.io>
2025-10-28 01:24:15 +0800

    feat: running in background and system tray

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,10 @@
 use bitsrun_lib::{SrunClient, SrunLoginState, get_login_state};
-use log::debug;
+use log::{debug, warn};
 use reqwest::Client;
 use tauri::async_runtime::Mutex;
-use tauri::menu::{CheckMenuItemBuilder, MenuBuilder, SubmenuBuilder};
-// use tauri::tray::TrayIconBuilder;
-use tauri::{App, Manager, State};
+use tauri::menu::{CheckMenuItemBuilder, Menu, MenuItem};
+use tauri::tray::{TrayIconBuilder, TrayIconEvent};
+use tauri::{App, Manager, State, WindowEvent};
 use tauri_plugin_autostart::ManagerExt;
 #[cfg(not(dev))]
 use tauri_plugin_prevent_default::Flags;
@@ -87,31 +87,30 @@ async fn check_status() -> Result<ApiResponse<SrunLoginState, String>, ()> {
     }
 }
 
-fn setup_menu(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
+fn setup_tray(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
     let autostart_manager = app.autolaunch();
 
+    let item_show_window = MenuItem::with_id(app, "show_window", "显示窗口", true, None::<&str>)?;
     let item_auto_start = CheckMenuItemBuilder::new("自动启动")
         .id("auto_start")
         .checked(autostart_manager.is_enabled().unwrap_or(false))
         .build(app)?;
+    let item_quit = MenuItem::with_id(app, "quit", "退出", true, None::<&str>)?;
+    let menu = Menu::with_items(app, &[&item_show_window, &item_auto_start, &item_quit])?;
 
-    let menu_settings = SubmenuBuilder::new(app, "设置")
-        .item(&item_auto_start)
-        .build()?;
-
-    // let menu_dev =MenuItem::new(app, "设置")
-    //     .item(&item_auto_start)
-    //     .build()?;
-
-    let menu = MenuBuilder::new(app)
-        .items(&[&menu_settings])
-        .text("devtools", "Devtools")
-        .build()?;
-
-    app.set_menu(menu)?;
-
-    app.on_menu_event(
-        move |app: &tauri::AppHandle, event| match event.id().0.as_str() {
+    let _tray = TrayIconBuilder::new()
+        .icon(app.default_window_icon().unwrap().clone())
+        .menu(&menu)
+        .show_menu_on_left_click(false)
+        .on_menu_event(move |app, event| match event.id.as_ref() {
+            "show_window" => {
+                let app = app.app_handle();
+                if let Some(window) = app.get_webview_window("main") {
+                    let _ = window.unminimize();
+                    let _ = window.show();
+                    let _ = window.set_focus();
+                }
+            }
             "auto_start" => {
                 let autostart_manager = app.autolaunch();
 
@@ -121,17 +120,32 @@ fn setup_menu(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
                     autostart_manager.disable().unwrap()
                 }
             }
-
-            "devtools" => {
-                let window = app.get_webview_window("main").unwrap();
-                window.open_devtools();
+            "quit" => {
+                app.exit(0);
             }
-
             _ => {
-                println!("unexpected menu event");
+                warn!("menu item {:?} not handled", event.id);
             }
-        },
-    );
+        })
+        .on_tray_icon_event(|tray, event| match event {
+            TrayIconEvent::DoubleClick {
+                id: _,
+                position: _,
+                rect: _,
+                button: _,
+            } => {
+                let app = tray.app_handle();
+                if let Some(window) = app.get_webview_window("main") {
+                    let _ = window.unminimize();
+                    let _ = window.show();
+                    let _ = window.set_focus();
+                }
+            }
+            _ => {
+                println!("unhandled event {event:?}");
+            }
+        })
+        .build(app)?;
 
     Ok(())
 }
@@ -165,7 +179,6 @@ pub fn run() {
             .with_flags(
                 Flags::CONTEXT_MENU | Flags::PRINT | Flags::DOWNLOADS | Flags::RELOAD | Flags::FIND,
             )
-            .shortcut(KeyboardShortcut::new("F12"))
             .build();
         builder.plugin(prevent)
     };
@@ -173,14 +186,22 @@ pub fn run() {
     builder
         .invoke_handler(tauri::generate_handler![init, login, logout, check_status,])
         .setup(|app| {
-            // let tray = TrayIconBuilder::new()
-            //     .icon(app.default_window_icon().unwrap().clone())
-            //     .build(app)?;
-
-            setup_menu(app)?;
-
+            setup_tray(app)?;
             Ok(())
         })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .on_window_event(|window, event| {
+            if let WindowEvent::CloseRequested { api, .. } = event {
+                window.hide().unwrap();
+                api.prevent_close();
+            }
+        })
+        .build(tauri::generate_context!())
+        .expect("error while building tauri application")
+        .run(|_app, event| {
+            if let tauri::RunEvent::ExitRequested { api, code, .. } = event
+                && code.is_none()
+            {
+                api.prevent_exit();
+            }
+        });
 }
