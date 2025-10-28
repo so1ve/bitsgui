fd85f02a6f5031d9bbf9440f833faaacc2be219a
so1ve <i@mk1.io>
2025-10-28 19:44:53 +0800

    chore: remove useless condition

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,9 +1,3 @@
 fn main() {
-    #[cfg(feature = "clippy")]
-    {
-        println!("cargo:warning=Skipping tauri_build during Clippy");
-    }
-
-    #[cfg(not(feature = "clippy"))]
     tauri_build::build();
 }
